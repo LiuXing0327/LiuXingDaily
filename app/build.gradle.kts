d7807bcf98ee4a3cf3a1a1acd9f8bf74502a04a6
RaoXin <1926879119@qq.com>
2025-09-05 22:25:37 +0800

    Fixed a crash when pressing the back key on some pages Fixed an issue with toolbar interaction when long-pressing the search view Adapt to EdgeToEdge mode,the interface is more immersive Fixed the issue of image and video interfaces jumping when entering and exiting immersive mode Modify the layout style of mood and weather Fixed the crash by retuning to the WebDav Interface Modify the corner radius of the gallery card to 12dp Adapt to predictive return gestures on Android 13 and above Adjust the unlock interface layout Optimize the loading speed of the daily list Fixed the issue where the newly added content was not refreshed correctly when rewriting a saved diary Modified the daily title style Added User Agreement and Privacy Policy A privacy pop-up window will pop up when you start it for the first time: If you agree, you can use it normally, otherwise you will exit Fixed the issue where extra spaces appeared when editing a diary containing audio When deleting audio or video placeholders in text, the corresponding resources can be deleted correctly Fixed the issue where the right border of the audio placeholder was not fully displayed Fixed an issue where the featured video image in the text was offset to the right Fixed an issue where edited content lost its pinned status after saving

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.liuxing.daily"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "2.7"
+        versionCode = 18
+        versionName = "2.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
