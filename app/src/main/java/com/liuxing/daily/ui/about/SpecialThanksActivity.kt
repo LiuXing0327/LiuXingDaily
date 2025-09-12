a748b03a42fde4c7be932ca76dfbc9e8e1a12b3c
RaoXin <1926879119@qq.com>
2025-09-12 21:16:55 +0800

    Delete open source libraries

--- app/src/main/java/com/liuxing/daily/ui/about/SpecialThanksActivity.kt
+++ app/src/main/java/com/liuxing/daily/ui/about/SpecialThanksActivity.kt
@@ -1,23 +1,18 @@
 package com.liuxing.daily.ui.about
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.liuxing.daily.R
-import com.liuxing.daily.adapter.SpecialThanksAdapter
 import com.liuxing.daily.data.SpecialThanksData
 import com.liuxing.daily.databinding.ActivitySpecialThanksBinding
 import com.liuxing.daily.util.ThemeUtil
 
 
-class SpecialThanksActivity : AppCompatActivity() {
+class SpecialThanksActivity : BaseSpecialThanksActivity() {
 
     private lateinit var specialThanksBinding: ActivitySpecialThanksBinding
-    private lateinit var specialThanksAdapter: SpecialThanksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,26 +46,10 @@ class SpecialThanksActivity : AppCompatActivity() {
         specialThanksBinding.toolbar.title = getString(R.string.special_thanks)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        finish()
-        return super.onOptionsItemSelected(item)
-    }
-
     /**
      * 初始化列表
      */
     private fun initRecyclerView() {
-        val linearLayoutManager = LinearLayoutManager(this)
-        specialThanksBinding.recyclerView.layoutManager = linearLayoutManager
-        specialThanksAdapter = SpecialThanksAdapter()
-        specialThanksBinding.recyclerView.adapter = specialThanksAdapter
-        setRecyclerData()
-    }
-
-    /**
-     * 设置列表数据
-     */
-    private fun setRecyclerData() {
         val specialThanksDataList = setOf(
             SpecialThanksData(
                 "zoyonsheng",
@@ -89,106 +68,11 @@ class SpecialThanksActivity : AppCompatActivity() {
                 "对醒悟推广的支持与帮助",
                 "",
                 0
-            ),
-
-            SpecialThanksData(
-                "Gson", "\nCopyright 2008 Google Inc.\n" +
-                        "\n" +
-                        "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                        "you may not use this file except in compliance with the License.\n" +
-                        "You may obtain a copy of the License at\n" +
-                        "\n" +
-                        "    http://www.apache.org/licenses/LICENSE-2.0\n" +
-                        "\n" +
-                        "Unless required by applicable law or agreed to in writing, software\n" +
-                        "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                        "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                        "See the License for the specific language governing permissions and\n" +
-                        "limitations under the License.\n",
-                "https://github.com/google/gson",
-                1
-            ),
-            SpecialThanksData(
-                "OkHttp", "\nCopyright 2019 Square, Inc.\n" +
-                        "\n" +
-                        "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                        "you may not use this file except in compliance with the License.\n" +
-                        "You may obtain a copy of the License at\n" +
-                        "\n" +
-                        "   http://www.apache.org/licenses/LICENSE-2.0\n" +
-                        "\n" +
-                        "Unless required by applicable law or agreed to in writing, software\n" +
-                        "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                        "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                        "See the License for the specific language governing permissions and\n" +
-                        "limitations under the License.\n",
-                "https://github.com/square/okhttp",
-                1
-            ),
-            SpecialThanksData(
-                "Glide",
-                "\nBSD, part MIT and Apache 2.0.\n",
-                "https://github.com/bumptech/glide",
-                1
-            ),
-            SpecialThanksData("PhotoView","\nCopyright 2018 Chris Banes\n" +
-                    "\n" +
-                    "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                    "you may not use this file except in compliance with the License.\n" +
-                    "You may obtain a copy of the License at\n" +
-                    "\n" +
-                    "   http://www.apache.org/licenses/LICENSE-2.0\n" +
-                    "\n" +
-                    "Unless required by applicable law or agreed to in writing, software\n" +
-                    "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                    "See the License for the specific language governing permissions and\n" +
-                    "limitations under the License.\n",
-                "https://github.com/Baseflow/PhotoView",
-                1
-            ),
-            SpecialThanksData(
-                "subsampling-scale-image-view",
-                "\nCopyright 2018 David Morrissey, and licensed under the Apache License, " +
-                        "Version 2.0. No attribution is necessary but it's very much appreciated. Star this project if you like it!\n",
-                "https://github.com/davemorrissey/subsampling-scale-image-view",
-                1
-            ),
-
-            SpecialThanksData(
-                "sardine.android",
-                "\nApache 2.0 License.\n",
-                "https://github.com/thegrizzlylabs/sardine-android",
-                1
-            ),
-
-            SpecialThanksData(
-                "zip4j",
-                "\nApache 2.0 License.\n",
-                "https://github.com/srikanth-lingala/zip4j",
-                1
-            ),
-
-            SpecialThanksData(
-                "ColorPickerView",
-                "\nCopyright [2025] [LiuXing]\n" +
-                        "\n" +
-                        "   Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                        "   you may not use this file except in compliance with the License.\n" +
-                        "   You may obtain a copy of the License at\n" +
-                        "\n" +
-                        "       http://www.apache.org/licenses/LICENSE-2.0\n" +
-                        "\n" +
-                        "   Unless required by applicable law or agreed to in writing, software\n" +
-                        "   distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                        "   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                        "   See the License for the specific language governing permissions and\n" +
-                        "   limitations under the License.\n",
-                "https://github.com/LiuXing0327/ColorPickerView",
-                1
             )
         )
-        specialThanksAdapter.setSpecialThanksList(specialThanksDataList.toList())
+        (this as BaseSpecialThanksActivity).initRecyclerView(
+            specialThanksBinding.recyclerView,
+            specialThanksDataList.toList()
+        )
     }
-
 }
