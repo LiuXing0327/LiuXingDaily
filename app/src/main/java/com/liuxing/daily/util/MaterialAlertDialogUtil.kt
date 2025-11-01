fa5373ec26097bc3e3f6e8ce13d4dc9e1285726a
RaoXin <1926879119@qq.com>
2025-11-01 10:00:47 +0800

    Update the parameters of the showDialog function

--- app/src/main/java/com/liuxing/daily/util/MaterialAlertDialogUtil.kt
+++ app/src/main/java/com/liuxing/daily/util/MaterialAlertDialogUtil.kt
@@ -6,6 +6,8 @@ package com.liuxing.daily.util
 
 import android.app.Activity
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -14,33 +16,52 @@ object MaterialAlertDialogUtil {
     /**
      * 显示对话框
      *
+     * 当 [layoutRes] 不为 null 时，消息内容 [message] 将被忽略。
+     *
      * @param context 上下文
-     * @param message 消息
+     * @param title 标题
+     * @param message 消息；若指定了 [layoutRes]，则忽略该参数
+     * @param layoutRes 自定义布局资源 ID
      * @param positiveText [onPositive]文本
      * @param onPositive setPositiveButton的回调函数
      * @param negativeText [onNegative]文本
      * @param onNegative setNegativeButton的回调函数
      * @param neutralText [onNeutral]文本
      * @param onNeutral setNeutralButton的回调函数
+     * @param onViewCreated 当使用自定义布局时，会在布局创建完成后回调函数，
+     *                      允许在此操作 view 或设置监听。
+     *
      * @return MaterialAlertDialog
      */
     fun showDialog(
         context: Context,
-        message: String,
+        title: String? = null,
+        message: String? = null,
+        layoutRes: Int? = null,
         positiveText: String = "",
         onPositive: (() -> Unit)? = null,
         negativeText: String = "",
         onNegative: (() -> Unit)? = null,
         neutralText: String = "",
-        onNeutral: (() -> Unit)? = null
+        onNeutral: (() -> Unit)? = null,
+        onViewCreated: ((View, AlertDialog) -> Unit)? = null
     ): AlertDialog? {
         val activity = (context as? Activity) ?: return null
         if(activity.isFinishing || activity.isDestroyed){
             LogUtil.w("showDialog: null activity")
             return null
         }
+
+        var view: View? = null
+
         MaterialAlertDialogBuilder(activity).apply {
-            setMessage(message)
+            setTitle(title)
+            if (layoutRes != null) {
+                view = LayoutInflater.from(context).inflate(layoutRes, null)
+                setView(view)
+            } else {
+                setMessage(message)
+            }
             setPositiveButton(positiveText) { _, _ ->
                 onPositive?.invoke()
             }
@@ -52,6 +73,10 @@ object MaterialAlertDialogUtil {
             }
             val dialog = create()
             dialog.show()
+
+            if (view != null) {
+                onViewCreated?.invoke(view, dialog)
+            }
             return dialog
         }
     }
