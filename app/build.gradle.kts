81e631e9a9e1650fc4d35fb87e714ef115455599
RaoXin <1926879119@qq.com>
2025-11-28 22:03:42 +0800

    Update versionCode to 22 Update versionName to 2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.liuxing.daily"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.8.3"
+        versionCode = 22
+        versionName = "2.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
