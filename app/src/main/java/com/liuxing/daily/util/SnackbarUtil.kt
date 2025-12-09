b4fe08a7f08cb83e06b0197efdd429d301ec750c
RaoXin <1926879119@qq.com>
2025-12-09 19:33:54 +0800

    Add set the anchor point and class note

--- app/src/main/java/com/liuxing/daily/util/SnackbarUtil.kt
+++ app/src/main/java/com/liuxing/daily/util/SnackbarUtil.kt
@@ -1,8 +1,12 @@
 package com.liuxing.daily.util
 
 import android.view.View
+import com.google.android.material.floatingtoolbar.FloatingToolbarLayout
 import com.google.android.material.snackbar.Snackbar
 
+/**
+ * Snackbar 工具类
+ */
 object SnackbarUtil {
 
     /**
@@ -12,6 +16,13 @@ object SnackbarUtil {
      * @param message 要显示的文本
      */
     fun showSnackbarShort(view: View?, message: String?) {
-        Snackbar.make(view!!, message!!, Snackbar.LENGTH_SHORT).show()
+        val snackbar = Snackbar.make(view!!, message!!, Snackbar.LENGTH_SHORT)
+
+        // 如果传入的 View 是 FloatingToolbarLayout，则设置锚点。
+        if (view is FloatingToolbarLayout) {
+            snackbar.setAnchorView(view)
+        }
+
+        snackbar.show()
     }
 }
