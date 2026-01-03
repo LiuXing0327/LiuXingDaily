b77c70bca29f4edb7ca818d34af4c86aece8b745
RaoXin <1926879119@qq.com>
2026-01-03 20:08:23 +0800

    Update vc to 24 Update vn to 2.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.liuxing.daily"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.9.1"
+        versionCode = 24
+        versionName = "2.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
