153d62a15e52ac3f5be8d0ecdcc9eb9d27d12f95
RaoXin <1926879119@qq.com>
2026-04-11 15:40:41 +0800

    Process more tags

--- app/src/main/java/com/liuxing/daily/util/TextUtil.kt
+++ app/src/main/java/com/liuxing/daily/util/TextUtil.kt
@@ -9,6 +9,33 @@ import android.text.style.ImageSpan
 
 object TextUtil {
 
+    private val mediaTagRegexes = listOf(
+        Regex("<\\s*img\\b[^>]*>", RegexOption.IGNORE_CASE),
+        Regex("<\\s*video\\b[^>]*>", RegexOption.IGNORE_CASE),
+        Regex("<\\s*audio\\b[^>]*>", RegexOption.IGNORE_CASE),
+    )
+
+    private val unwrapTagRegexes = listOf(
+        Regex("<b>([\\s\\S]*?)</b>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<i>([\\s\\S]*?)</i>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<u>([\\s\\S]*?)</u>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<s>([\\s\\S]*?)</s>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<color=[^>]+>([\\s\\S]*?)</color>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<bg=[^>]+>([\\s\\S]*?)</bg>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<size=[^>]+>([\\s\\S]*?)</size>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<a\\s+href=\"[^\"]*\"\\s*>([\\s\\S]*?)</a>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<blockquote>([\\s\\S]*?)</blockquote>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<align=(left|center|right)\\s*>([\\s\\S]*?)</align\\s*>", RegexOption.IGNORE_CASE) to "$2",
+        Regex("<h([123])\\s*>([\\s\\S]*?)</h\\1\\s*>", RegexOption.IGNORE_CASE) to "$2",
+        Regex("<li\\s+type=\"ul\"\\s*>([\\s\\S]*?)</li>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<li\\s+type=\"ol\"(?:\\s+index=\"[^\"]*\")?\\s*>([\\s\\S]*?)</li>", RegexOption.IGNORE_CASE) to "$1",
+        Regex("<todo\\s+checked=\"(true|false)\"\\s*>([\\s\\S]*?)</todo>", RegexOption.IGNORE_CASE) to "$2",
+    )
+
+    private val separatorTagRegexes = listOf(
+        Regex("<hr\\s*/?>", RegexOption.IGNORE_CASE),
+    )
+
     /**
      * 获取字数
      *
@@ -16,11 +43,7 @@ object TextUtil {
      * @return 字数
      */
     fun getWordCount(text: String): Int {
-        val originalText = text
-        val textWithoutImages = originalText.replace(Regex("<\\s*img[^>]*>"), "")
-        val textWithoutVideo = textWithoutImages.replace(Regex("<\\s*video[^>]*>"), "")
-        val textWithoutAudio = textWithoutVideo.replace(Regex("<\\s*audio[^>]*>"), "")
-        val spannableText = SpannableString(textWithoutAudio)
+        val spannableText = SpannableString(stripRichTextTags(text, mediaReplacement = "", separatorReplacement = ""))
         val imageSpans = spannableText.getSpans(0, spannableText.length, ImageSpan::class.java)
         var totalLength = spannableText.length
         for (imageSpan in imageSpans) {
@@ -40,15 +63,25 @@ object TextUtil {
      * @return 替换结果
      */
     fun replaceTag(text: String, replacement: String = "..."): String {
-        val imageRegex = Regex("<img\\s+src=\"(.*?)\"\\s*/?>", RegexOption.IGNORE_CASE)
-        val videoRegex = Regex("<video\\s+src=\"(.*?)\"\\s*/?>", RegexOption.IGNORE_CASE)
-        val audioRegex = Regex("<audio\\s+src=\"(.*?)\"\\s*/?>", RegexOption.IGNORE_CASE)
-        var replacedText = text
-        replacedText = replacedText.replace(imageRegex, replacement)
-        replacedText = replacedText.replace(videoRegex, replacement)
-        replacedText = replacedText.replace(audioRegex, replacement)
+        return stripRichTextTags(text, mediaReplacement = replacement, separatorReplacement = replacement)
+    }
 
-        return replacedText
+    private fun stripRichTextTags(
+        text: String,
+        mediaReplacement: String,
+        separatorReplacement: String
+    ): String {
+        var strippedText = text
+        mediaTagRegexes.forEach { regex ->
+            strippedText = strippedText.replace(regex, mediaReplacement)
+        }
+        separatorTagRegexes.forEach { regex ->
+            strippedText = strippedText.replace(regex, separatorReplacement)
+        }
+        unwrapTagRegexes.forEach { (regex, replacement) ->
+            strippedText = strippedText.replace(regex, replacement)
+        }
+        return strippedText
     }
 
 }
